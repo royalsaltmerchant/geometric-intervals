35c785cdf2efc143bc5df20a6ae9e9e248848639
Julian ranieri <julianranieri@Julians-MBP.lan>
2022-07-25 12:31:54 -0600

    relationships to tonic etc

--- dom.js
+++ dom.js
@@ -1,7 +1,24 @@
 var width = window.innerHeight
 var height = window.innerHeight
 
+
 function Dom() {
+  this.tonic = 1000
+
+  this.renderTonic = function() {
+    var elem = document.getElementById('tonic')
+    var input = document.createElement('input')
+    input.value = this.tonic
+    input.type = "number"
+    input.min = '0'
+    input.max = '40000'
+    input.addEventListener('change', (e) => {
+      dom.tonic = parseInt(e.target.value)
+      update()
+    })
+
+    elem.appendChild(input)
+  }
 
   this.renderNodeAlphabet = function(nodes) {
     // clear node alphabet on canvas
@@ -67,14 +84,17 @@ function Dom() {
   }
 
   this.renderLineData = function(lines, nodes, ctx) {
-    // clear line distance
-    for(var input of document.querySelectorAll(".line-distance")) input.remove()
+    // clear line lengths
+    for(var input of document.querySelectorAll(".line-length")) input.remove()
+    // clear line relationships
+    for(var input of document.querySelectorAll(".line-relationship")) input.remove()
 
     var lineElements = []
+    var lineLengths = []
 
     lines.forEach((line, index) => {
-      // calculate distance of line
-      var distance = Math.floor(Math.hypot(line.node1.x - line.node2.x, line.node1.y - line.node2.y))
+      // calculate length of line
+      var length = Math.floor(Math.hypot(line.node1.x - line.node2.x, line.node1.y - line.node2.y))
       // get nodes in line alphebetized 
       var nodesInLine = nodes.filter(node => ctx.isPointInPath(node.path, line.node1.x, line.node1.y) || ctx.isPointInPath(node.path, line.node2.x, line.node2.y))
       var firstNodeAlpha = getAlphaFromIndex(nodes.indexOf(nodesInLine[0]))
@@ -82,10 +102,11 @@ function Dom() {
       var nodesAlphabetized = [firstNodeAlpha, secondNodeAlpha].sort()
       // create dom element
       var elem = document.createElement('div')
-      elem.classList.add('line-distance')
+      elem.classList.add('line-length')
       elem.accessKey = nodesAlphabetized[0] + nodesAlphabetized[1]
-      elem.innerText = `${nodesAlphabetized[0]} → ${nodesAlphabetized[1]} = ${distance}`
+      elem.innerText = `${nodesAlphabetized[0]} → ${nodesAlphabetized[1]} = ${length}`
       lineElements.push(elem)
+      lineLengths.push({node1: nodesAlphabetized[0], node2: nodesAlphabetized[1], length: length})
     })  
     // sort alphabetically
     lineElements.sort(function(a, b){
@@ -93,10 +114,36 @@ function Dom() {
       if(a.accessKey > b.accessKey) { return 1; }
       return 0;
     })
-    // render
-    lineElements.forEach(elem => document.getElementById('lines').appendChild(elem))
+    // render line lengths
+    lineElements.forEach(elem => document.getElementById('lengths').appendChild(elem))
+
+    var lineRelationships = []
+    // Line relationships
+    lineLengths.forEach(firstLength => {
+      lineLengths.forEach(secondLength => {
+        if(firstLength === secondLength) return
+        else {
+          if(firstLength.length > secondLength.length) {
+            var firstDividedBySecond = (firstLength.length / secondLength.length).toFixed(2)
+            var elem = document.createElement('div')
+            elem.classList.add('line-relationship')
+            elem.accessKey = firstDividedBySecond
+            elem.innerText = `
+              ${firstLength.node1}-${firstLength.node2} / ${secondLength.node1}-${secondLength.node2} = ${firstDividedBySecond} (${(firstDividedBySecond * dom.tonic).toFixed(2)}hz)
+            `
+            lineRelationships.push(elem)
+          }
+        }
+      })
+    })
+
+    lineRelationships.sort((a, b) => a.accessKey - b.accessKey)
+    lineRelationships.forEach(elem => {
+      document.getElementById('relationships').appendChild(elem)
+    })
   }
 
+  this.renderTonic()
   return this
 }
 
